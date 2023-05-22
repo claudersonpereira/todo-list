2354091ba531f69efa30714fd196d5afa5835e56
Clauderson Pereira <claudersonsp@gmail.com>
2023-05-22 18:08:35 -0300

    estrutura inicial do projeto

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+
+function addNewTask() {
+    var newTaskWrapper = document.getElementById("txtNewTask");
+    var newTask = newTaskWrapper.value;
+
+    var newTaskItem = document.createElement("LI");
+    var txtTask = document.createTextNode(newTask);
+    newTaskItem.appendChild(txtTask);
+
+    var tasksListWrapper = document.getElementById("tasksList");
+
+    if(!newTask == ""){
+        tasksListWrapper.appendChild(newTaskItem);
+    }
+    
+    newTaskWrapper.value = "";
+}
+
+// Add a "checked" symbol when clicking on a list item
+var tasksListWrapper = document.getElementById("tasksList");
+tasksListWrapper.addEventListener('click', function(e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+  }
+}, false);
